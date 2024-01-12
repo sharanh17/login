f67be3b77ea36b13afec2ec0bfdc1320d6271681
sharanh17 <sharanhidaduggi17@gmail.com>
2024-01-12 14:56:19 +0530

    first

---
+++ src/LoginPage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username === "user" && password === "password") {
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {isSubmitted ? (
+        <div>
+          <p>Welcome, {username}!</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
